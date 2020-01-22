ad5556968934dd7a5700784feb3ff4d4c033054b
yingting <ivylovefreja@gmail.com>
2020-01-22 21:57:57 +0800

    evaluation-course-0122更新

--- js/js-yusan.js
+++ js/js-yusan.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
 
-	//響應式下拉式選單
+	//主頁響應式下拉式選單
 	$('.showmenu').on('click', function(e){
 		e.preventDefault();
 		$('body').toggleClass('menu-show');
+	});
+
+	//課程響應式下拉選單
+	$('.course-showmenu').on('click', function(e){
+		e.preventDefault();
+		$('body').toggleClass('course-menu-show');
 	});
 
 });
